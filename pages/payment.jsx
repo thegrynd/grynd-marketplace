7c4635d28db2c41a9be5c3f6c2cfe19f30af50d7
EOEboh <ecolejnr007@gmail.com>
2023-03-31 08:39:43 +0100

    weekly commits

--- pages/payment.jsx
+++ pages/payment.jsx
@@ -2,17 +2,48 @@ import { Grid } from "@mui/material";
 import PaymentForm from "pages-sections/payment/PaymentForm";
 import PaymentSummary from "pages-sections/payment/PaymentSummary";
 import CheckoutNavLayout from "components/layouts/CheckoutNavLayout";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 const Checkout = () => {
-  return <CheckoutNavLayout>
+  const promise = loadStripe(
+    "pk_test_51MidNoKdfAkbgNXX4KGIrxHvVVjVVjjlDBSyUNEYxLzwjg8IP4uV1sNDy6noJWDGJMR5Rj3LA0qt56ZRIHQpstTw00amCEEvsq"
+  );
+  return (
+    <CheckoutNavLayout>
       <Grid container flexWrap="wrap-reverse" spacing={3}>
         <Grid item lg={8} md={8} xs={12}>
-          <PaymentForm />
+          <Elements stripe={promise}>
+            <PaymentForm />
+          </Elements>
         </Grid>
 
         <Grid item lg={4} md={4} xs={12}>
           <PaymentSummary />
         </Grid>
       </Grid>
-    </CheckoutNavLayout>;
+    </CheckoutNavLayout>
+  );
 };
 export default Checkout;
+
+// import React from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+// import CheckoutForm from "./CheckoutForm";
+// import "./App.css";
+
+// // Make sure to call loadStripe outside of a component’s render to avoid
+// // recreating the Stripe object on every render.
+// // This is your test publishable API key.
+// const promise = loadStripe("pk_test_51MidNoKdfAkbgNXX4KGIrxHvVVjVVjjlDBSyUNEYxLzwjg8IP4uV1sNDy6noJWDGJMR5Rj3LA0qt56ZRIHQpstTw00amCEEvsq");
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Elements stripe={promise}>
+//         <CheckoutForm />
+//       </Elements>
+//     </div>
+//   );
+// }
